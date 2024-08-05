978df5c7eb3b8f9e9d4a8dc0a4af182c47eb81bb
EmanuelMVera <emanuel_vera@live.com.ar>
2024-08-05 04:08:11 -0300

    configuracion para levantar server con render 1

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,9 +1,8 @@
-import "dotenv/config"; // Asegúrate de que dotenv cargue las variables de entorno
+import "dotenv/config";
 import app from "./src/app.mjs";
 
-const PORT = process.env.PORT || 3001; // Usa el valor de PORT de las variables de entorno o un valor por defecto
-const NODE_ENV = process.env.NODE_ENV || "development"; // Define un valor por defecto
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,5 +1,4 @@
 import express, { urlencoded, json } from "express";
-import morgan from "morgan";
 import cors from "cors";
 import routes from "./routes/index.mjs";
 import errorHandler from "./middleware/errorHandler.mjs";
@@ -9,18 +8,14 @@ const app = express();
 app.use(urlencoded({ extended: true, limit: "10mb" }));
 app.use(json({ limit: "10mb" }));
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
 app.use(
   cors({
-    origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+    origin: "https://weatherproject-yfkx.onrender.com",
     credentials: true,
   })
 );
-app.use("/", routes);
 
+app.use("/", routes);
 app.use(errorHandler);
 
 export default app;
